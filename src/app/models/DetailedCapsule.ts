d3fc8361530e9e481a64005a102b1b8e503ad068
jeremy.marchand <jeremy.marchand@batiment.setec.fr>
2018-06-06 12:06:34 +0200

    Add detailed capsule service & model (Close #15)

---
+++ src/app/models/DetailedCapsule.ts
@@ -0,0 +1,10 @@
+export interface DetailedCapsule {
+    capsule_serial: string;
+    capsule_id: string;
+    status: string;
+    original_launch: any;
+    missions: string[];
+    landings: number;
+    type: string;
+    details: string;
+}
