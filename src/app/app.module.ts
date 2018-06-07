8cfd5f1b2ad69e7ff1bf1187de691026531d96c8
Amadou Diop <d.amadoudiop@gmail.com>
2018-06-07 12:43:50 +0200

    company page info

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,22 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {NgModule, NO_ERRORS_SCHEMA} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { MaterialModule } from './modules/material/material.module';
-import { AppRoutingModule } from './modules/app-routing/app-routing.module';
-import { FormsModule } from '@angular/forms';
-import { HomeComponent } from './pages/home/home.component';
-import { SpacexApiService } from './services/spacex-api.service';
-import { HttpClientModule } from '@angular/common/http';
-import { LaunchesListComponent } from './pages/launches-list/launches-list.component';
+import {AppComponent} from './app.component';
+import {MaterialModule} from './modules/material/material.module';
+import {AppRoutingModule} from './modules/app-routing/app-routing.module';
+import {FormsModule} from '@angular/forms';
+import {HomeComponent} from './pages/home/home.component';
+import {SpacexApiService} from './services/spacex-api.service';
+import {HttpClientModule} from '@angular/common/http';
+import {LaunchesListComponent} from './pages/launches-list/launches-list.component';
 import {MatToolbarModule} from '@angular/material';
 import {MDBBootstrapModule} from 'angular-bootstrap-md';
-import { TopbarComponent } from './components/topbar/topbar.component';
-import { FooterComponent } from './components/footer/footer.component';
-import { GmapsComponent } from './components/gmaps/gmaps.component';
-import { AgmCoreModule } from '@agm/core';
+import {TopbarComponent} from './components/topbar/topbar.component';
+import {FooterComponent} from './components/footer/footer.component';
+import {GmapsComponent} from './components/gmaps/gmaps.component';
+import {CompanyComponent} from './components/company/company.component';
+import {AgmCoreModule} from '@agm/core';
 
 @NgModule({
   declarations: [
@@ -25,6 +26,7 @@ import { AgmCoreModule } from '@agm/core';
     TopbarComponent,
     FooterComponent,
     GmapsComponent,
+    CompanyComponent,
   ],
   imports: [
     AppRoutingModule,
@@ -41,6 +43,7 @@ import { AgmCoreModule } from '@agm/core';
   ],
   providers: [SpacexApiService],
   bootstrap: [AppComponent],
-  schemas: [ NO_ERRORS_SCHEMA ]
+  schemas: [NO_ERRORS_SCHEMA]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/modules/app-routing/app-routing.module.ts
+++ src/app/modules/app-routing/app-routing.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from '../../pages/home/home.component';
 import { LaunchesListComponent } from '../../pages/launches-list/launches-list.component';
+import { CompanyComponent } from '../../pages/company/company.component';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'launches', component: LaunchesListComponent },
+  { path: 'company', component: CompanyComponent },
 ];
 
 @NgModule({
