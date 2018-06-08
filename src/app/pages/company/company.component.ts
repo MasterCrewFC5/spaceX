1da6f935e14ee6a6d836f5f2a964db71fe948c9f
jeremy.marchand <jeremy.marchand@batiment.setec.fr>
2018-06-08 11:20:56 +0200

    Revert "Merge branch 'develop' of github.com:MasterCrewFC5/spaceX into develop"
    This reverts commit 30b0143e79a1f6554de7ddf443bf0c4ce2af1b7b, reversing
    changes made to fa8434fee35b7c52e987273b998fe8f2c9429be5.

--- src/app/pages/company/company.component.ts
+++ src/app/pages/company/company.component.ts
@@ -1,36 +1,35 @@
-import {Component, OnInit} from '@angular/core';
-import {CompanyService} from '../../services/company/company.service';
+import { Component, OnInit } from '@angular/core';
+import { CompanyService } from '../../services/company/company.service';
 import {Launch} from '../../models/Launch';
 
 @Component({
-    selector: 'app-company',
-    templateUrl: './company.component.html',
-    styleUrls: ['./company.component.css']
+  selector: 'app-company',
+  templateUrl: './company.component.html',
+  styleUrls: ['./company.component.css']
 })
 export class CompanyComponent implements OnInit {
 
-    company: any = [];
+  company: any = [];
 
-    error = '';
+  error = '';
 
-    constructor(private companyService: CompanyService) {
-    }
+  constructor(private companyService: CompanyService) { }
 
-    ngOnInit() {
-        this.getCompanyInfo();
-    }
+  ngOnInit() {
+    this.getCompanyInfo();
+  }
 
-    getCompanyInfo() {
-        this.companyService.getCompanyInfo().toPromise().then(
-            (result) => {
-                console.log(result);
-                this.company = result;
-            }
-        ).catch(
-            (er) => {
-                this.error = er;
-            }
-        );
-    }
+  getCompanyInfo() {
+    this.companyService.getCompanyInfo().toPromise().then(
+      (result) => {
+        console.log(result);
+        this.company = result;
+      }
+    ).catch(
+       (er) => {
+        this.error = er;
+      }
+    );
+  }
 
 }
